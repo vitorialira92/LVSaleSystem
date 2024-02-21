2e7a9998eb9bbb2a123c85349c69547f3d80f5fd
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-02-21 12:10:44 -0300

    feat: access token

---
+++ LVSaleSystem.API/AccessTokens/Token.cs
@@ -0,0 +1,21 @@
+﻿using LVSaleSystem.API.Model.Users;
+using System.ComponentModel.DataAnnotations;
+
+namespace LVSaleSystem.API.AccessTokens
+{
+    public class Token
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Value { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        [Required]
+        public DateTime ExpiresAt { get; set; }
+        [Required]
+        public UserRole Role { get; set; }
+        [Required]
+        public int UserId { get; set; }
+    }
+}
