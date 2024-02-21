db9a162a2f762d2d5deed5cc7fcd0bfdfb12f001
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-02-21 12:11:16 -0300

    feat: project config and structure

--- LVSaleSystem.API/Data/LVContext.cs
+++ LVSaleSystem.API/Data/LVContext.cs
@@ -1,19 +1,48 @@
-﻿using LVSaleSystem.API.Model.Users;
+﻿using LVSaleSystem.API.AccessTokens;
+using LVSaleSystem.API.Model.Products;
+using LVSaleSystem.API.Model.Stocks;
+using LVSaleSystem.API.Model.Transactions;
+using LVSaleSystem.API.Model.Users;
+using LVSaleSystem.API.Model.Users.Customers;
 using Microsoft.EntityFrameworkCore;
 
 namespace LVSaleSystem.API.Data
 {
     public class LVContext : DbContext
     {
-        public DbSet<UserDetails> UserDetails { get; set; }
-        public DbSet<Customer> Customer { get; set; }
-        public DbSet<Manager> Manager { get; set; }
+        public DbSet<Token> Tokens { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+        public DbSet<Wallet> Wallets { get; set; }
+        public DbSet<CreditCard> CreditCards { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<SellingItem> SellingsItems { get; set; }
+        public DbSet<Selling> Sellings { get; set; }
+        public DbSet<Clothing> Clothings { get; set; }
+
+        public DbSet<Return> Returns { get; set; }
+        public DbSet<ClothingStock> ClothingStocks { get; set; }
         public DbSet<UserDetails> UserDetail { get; set; }
 
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public LVContext(DbContextOptions options) : base(options)
         {
-            optionsBuilder.UseSqlite("Data Source=localdatabase.db");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Customer>().HasIndex(u => u.CPF)
+                .IsUnique();
+            modelBuilder.Entity<Token>().HasIndex(u => u.Value)
+                .IsUnique();
+            modelBuilder.Entity<Clothing>();
+
+            //modelBuilder.Entity<SellingItem>().OwnsOne(p => p.Product);
+            //modelBuilder.Entity<ClothingStock>().OwnsOne(p => p.Item);
+
+
         }
 
     }
--- LVSaleSystem.API/Program.cs
+++ LVSaleSystem.API/Program.cs
@@ -1,4 +1,12 @@
 
+using LVSaleSystem.API.Data;
+using LVSaleSystem.API.Filters;
+using LVSaleSystem.API.Repositories;
+using LVSaleSystem.API.Services;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 namespace LVSaleSystem.API
 {
     public class Program
@@ -7,11 +15,50 @@ namespace LVSaleSystem.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddCors(corsOptions =>
+            {
+                corsOptions.AddPolicy("policyFlex", policyBuilder =>
+                {
+                    policyBuilder.WithOrigins("http://localhost:5500");
+                });
 
-            builder.Services.AddControllers();
+            });
+
+            builder.Services.AddScoped<IContentTypeProvider, FileExtensionContentTypeProvider>();
+            builder.Services.AddScoped<TokenRepository>();
+            builder.Services.AddScoped<TokenService>();
+
+            builder.Services.AddScoped<CustomerRepository>();
+            builder.Services.AddScoped<ProductsRepository>();
+            builder.Services.AddScoped<SellingRepository>();
+            builder.Services.AddScoped<ManagerRepository>();
+            builder.Services.AddScoped<ReturnsRepository>();
+
+            builder.Services.AddScoped<CustomerService>();
+            builder.Services.AddScoped<SellingService>();
+            builder.Services.AddScoped<ReturnsService>();
+            builder.Services.AddScoped<LoginService>();
+
+            builder.Logging.ClearProviders();
+
+            builder.Logging.AddConsole();
+
+            builder.Services.AddControllers(options =>
+            {
+                options.Filters.Add<ExceptionFilter>();
+                options.Filters.Add<LoggingAttributeFilter>();
+            });
+
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<LVContext>(options =>
+            {
+                options.UseLazyLoadingProxies()
+                .UseSqlite(builder.Configuration.GetConnectionString("LVSaleSystemSqlite"));
+            });
+
 
             var app = builder.Build();
 
@@ -21,10 +68,13 @@ namespace LVSaleSystem.API
                 app.UseSwaggerUI();
             }
 
+            app.UseCors("policyFlex");
+
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
+            app.UseStaticFiles();
 
             app.MapControllers();
 
